cd70aeceb04fe1b4036be6b3267f0dec035f7154
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-06-01 20:51:25 -0400

    6/1/22 progress in the course

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -9,22 +9,38 @@ public class SignUp {
         
 
         //Task 1 - Using Scanner, ask the user questions
-
         //Ask for their first name.
+        System.out.println("What is your first name?");
+        String firstName = scan.nextLine();
+
         //Ask for their last name.
+        System.out.println("What is your last name?");
+        String lastName = scan.nextLine();
+
         //Ask: how old are you?
+        System.out.println("What is your age?");
+        int age = scan.nextInt();
+
         /* Ask them to make a username. SIDE NOTE:
            – nextLine() gets "skipped" if you put it ahead of nextInt() , nextDouble(), nextLong().
            – You will understand why this happens when we cover Delimiters in the next section (Booleans and Conditionals).
            – For now, just know that the solution is to add an extra nextLine().
         */
-        //Ask what city they live in.
-        //Ask what country that's from.
+        scan.nextLine();
+        System.out.println("Please enter a username.");
+        String userName = scan.nextLine();
 
+        //Ask what city they live in.
+        System.out.println("What city do you live in?");
+        String city = scan.nextLine();
+
+        //Ask what country that's from.
+        System.out.println("What country do you live in?");
+        String country = scan.nextLine();
 
         //Task 2 - Print their information. 
+        System.out.println("Thank you " + firstName + lastName + " for joining JavaGram! Your new username is: " + userName + ". Hows the wheather in " + city + ", " + country + "?");
 
-        System.out.println("Thank you for joining JavaGram!");
 
         //Print their information like so:
 
@@ -39,5 +55,6 @@ public class SignUp {
 
 
         //close scanner. It's good practice :D ! 
+        scan.close();
     }
 }
